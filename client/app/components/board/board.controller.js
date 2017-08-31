fc998b25532f5f86f716c1b27088382923b08a0e
jmaicaaan <jmsantosss29@yahoo.com>
2017-09-01 00:11:51 +0800

    Box can be click

--- client/app/components/board/board.controller.js
+++ client/app/components/board/board.controller.js
@@ -3,17 +3,31 @@ class BoardController {
     "ngInject";
     this.name = 'board';
     this.boxes = [];
+    this.clickedBoxes = [];
+    this.totalNumberOfBox = 9;
     this.init();
   }
 
   init = () => {
-    this.initializeBox()
+    this.initializeBox();
   };
 
   initializeBox = () => {
-    let len = 9;
-    for (let i = 1; i <= len; i++) {
-      this.boxes.push(i);
+    for (let i = 1; i <= this.totalNumberOfBox; i++) {
+      this.boxes.push({
+        boxNo: i,
+        isClicked: false
+      });
+    }
+  };
+
+  onBoxClick = (box) => {
+    if (!box) return;
+    if (this.clickedBoxes.length <= this.totalNumberOfBox && !box.isClicked) {
+      box.isClicked = true;
+      this.clickedBoxes.push(box);
+    } else {
+      alert('Box cannot be clicked');
     }
   };
 }
